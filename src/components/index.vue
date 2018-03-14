706608b407264c227a544947be17dbe98b20397d
gengyanan <879218086@qq.com>
2018-03-14 10:27:22 +0800

    add goodsShop

---
+++ src/components/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <span v-once>{{message}}</span>
+        <div v-once>
+            <span>{{message}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "index",
+        data(){
+            return{
+                message:'nnisss'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
